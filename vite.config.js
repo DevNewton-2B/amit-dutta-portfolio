e75fcb3134997faf0c02af572c1156b75d1bef73
CODEBOT <pankoustav@gmail.com>
2026-05-26 04:15:57 +0530

    Vite Update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Add this base path for GitHub Pages deployment
-  base: '/amit-dutta-portfolio/', 
+  base: '/amit-dutta-portfolio/',
+  
+  // ADD THIS BUILD BLOCK
+  build: {
+    chunkSizeWarningLimit: 1000, // Increases the limit to 1000kB (1MB)
+  }
 })
